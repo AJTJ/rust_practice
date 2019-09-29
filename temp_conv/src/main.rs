b0199945e496752863d694aebe86703d5cb4bfef
Thelonious Janke <janke.aaron@gmail.com>
2019-09-29 00:28:10 -0700

    feat: fib num calculator

--- temp_conv/src/main.rs
+++ temp_conv/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     println!("50 C in F is {}", celc_far(50, false));
     println!("122 F in C is {}", celc_far(122, true));
+
+    let fib_num = 20;
+    println!("{} fib number is {}", fib_num, gen_fib(20));
 }
 
 fn celc_far(temp: i32, is_f: bool) -> i32 {
@@ -10,4 +13,16 @@ fn celc_far(temp: i32, is_f: bool) -> i32 {
     } else {
         temp * 9 / 5 + 32
     }
+}
+
+fn gen_fib(target: i32) -> i32 {
+    let mut first_number = 0;
+    let mut second_number = 1;
+    for _ in 0..target - 2 {
+        let outcome = first_number + second_number;
+        first_number = second_number;
+        second_number = outcome;
+    }
+
+    return second_number;
 }
