aab514058df651511c1c96e2b283f790550cd0fe
AJTJ <janke.aaron@gmail.com>
2020-06-18 00:13:09 -0700

    feat: changed letter casing in exercise

--- collections_exercises_2/src/main.rs
+++ collections_exercises_2/src/main.rs
@@ -1,11 +1,26 @@
+/*
+ TODO
+ - Take punctuation into account
+*/
+
 fn main() {
+    fn switch_casing(word: &str) -> String {
+        let (big, end) = word.split_at(1);
+        [&big.to_uppercase(), end].join("")
+    };
+
     fn alter_word(word: &str) -> String {
         let vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
         if word.chars().nth(0).map_or(false, |c| vowels.contains(&c)) {
             [word, "hay", " "].join("")
         } else {
-            let (first, last) = word.split_at(1);
-            [last, first, "ay", " "].join("")
+            let (first, mut last) = word.split_at(1);
+            let big_last;
+            if first.chars().all(char::is_uppercase) {
+                big_last = switch_casing(last);
+                last = &big_last
+            }
+            [last, &first.to_lowercase(), "ay", " "].join("")
         }
     };
 
